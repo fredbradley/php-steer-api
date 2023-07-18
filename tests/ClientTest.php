a046e3804077a51701c7612b67c61f02236b7944
Fred Bradley <code@fredbradley.co.uk>
2023-07-18 16:01:50 +0100

    start of some more test

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,9 +1,17 @@
 <?php
 
 it('connects to steer', function () {
+    $json = '{ "data": [{"sn_in": ""},{"gender":"m"}]}';
     // I've no idea what I'm doing here...
     $client = Mockery::mock(\FredBradley\PhpSteerApi\Client::class);
-    $client->shouldReceive('get');
+    $client->shouldReceive('get')
+        ->andReturn(json_decode($json));
+    $mockResponse = new \GuzzleHttp\Handler\MockHandler([
+        new \GuzzleHttp\Psr7\Response(200, [], $json),
+    ]);
+
+    expect($client->get(queryBuilder()))
+        ->toBeObject();
 });
 
 
