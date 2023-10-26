95cab1bd2e5804d396eca5c250425fee55b58db5
Fred Bradley <code@fredbradley.co.uk>
2023-10-26 10:00:01 +0100

    fix: namespace incorrect

--- src/Facades/Steer.php
+++ src/Facades/Steer.php
@@ -2,7 +2,7 @@
 
 namespace FredBradley\PhpSteerApi\Facades;
 
-class Steer extends Illuminate\Support\Facades\Facade
+class Steer extends \Illuminate\Support\Facades\Facade
 {
     protected static function getFacadeAccessor()
     {
